abffa865173fc3fb6b6e6d353906a6083b3ce99f
QooStore <102283537+QooStore@users.noreply.github.com>
2022-06-29 14:21:21 +0900

    Delete rentCar directory

--- rentCar/src/main/java/com/RentLoGo/member/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.RentLoGo.member.service;
-
-import com.RentLoGo.member.model.MemberDTO;
-
-public interface MemberService {
-
-	public void memberJoin(MemberDTO member) throws Exception;
-}
