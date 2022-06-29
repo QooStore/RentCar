dcac8ddad9f533ea321964cc77b9db3d6750e593
QooStore <102283537+QooStore@users.noreply.github.com>
2022-06-29 15:47:00 +0900

    Delete rentCar directory

--- rentCar/src/main/java/com/RentLoGo/rentCar/model/CarMapper.java
+++
@@ -1,8 +0,0 @@
-package com.RentLoGo.rentCar.model;
-
-import java.util.List;
-
-public interface CarMapper {
-
-	public List<CarDTO> selectAllrentCar();
-}
--- rentCar/src/main/java/com/RentLoGo/rentCar/model/CarService.java
+++
@@ -1,8 +0,0 @@
-package com.RentLoGo.rentCar.model;
-
-import java.util.List;
-
-public interface CarService {
-
-	public List<CarDTO> selectAllrentCar();
-}
